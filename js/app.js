27e23bb95b50aed406c00e4a73931117dc023012
Majka Karpinska <maja.miri@gmail.com>
2017-09-09 20:08:24 +0200

    rightbox animation - halfway done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 $(function () {
 
-  
+  // const x = new Promise((resolve, reject) => {
+  //
+  //     resolve($('.form2').addClass('slide-right'););
+  //
+  //
+  // });
+
+
+
+  $(window).on("load", (() => {
+    const slideForm = new Promise((resolve, reject) => {
+        resolve($('.form2').addClass('slide-right'));
+    });
+
+
+    slideForm.then(
+      setTimeout(function () {
+        
+      $("button").css("display", "inline-block")
+    }, 1000));
+
+
+	}));
 
 });
