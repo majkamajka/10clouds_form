3f60122203ae70750d2e8834bbb45ba684f1e5a7
Majka Karpinska <maja.miri@gmail.com>
2017-09-10 16:10:30 +0200

    form validation started

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function () {
 
+//animations
   const form = $('.form2');
   const btn = $("button");
   const formText = $('.form2 h2');
@@ -8,6 +9,62 @@ $(function () {
   const tabs = $('.tab');
   const bgTop = $('#gradient-lines');
   const linesBottom = $('#lines-bottom');
+
+//form
+  const name = $('#name');
+  const prefix = $('#prefix');
+  const tel = $('#tel-number');
+  // const gender
+  const dobDay = $('#dobDay');
+  const dobMonthNum = $('#dobMonth-num');
+  const dobMonthName = $('#dobMonth-name');
+  const dobYear = $('#dobYear');
+
+  $(name).on("change", () => {
+    $('.tel-warn').remove();
+
+    let warning = $("<span class='warning tel-warn'></span>");
+
+
+
+    if (name.val().length === 0) { // czy wpisane
+      warning.text("Please enter your name");
+      $(name).after(warning);
+    }
+
+    if (name.val().length >50) { // czy nie za długie
+      console.log("name cannot be longet than 50 chars");
+    }
+
+    if (name.val().indexOf(' ') > 0 && name.val().indexOf(' ') < name.val().length -2) { // czy jest spacja w środku
+      console.log("ok");
+    } else {
+      console.log("please enter both first and second name");
+    }
+  });
+
+
+  $(prefix).on("change", () => console.log(prefix.val()));
+
+  $(tel).on("change", () => {
+    let tel2 = tel.val();
+    tel2 = tel2.split(" ").join("");
+    tel2 = tel2.split("-").join("");
+    if (tel2.length != 9) {
+      console.log("Please enter valid phone number (9 characters)");
+    }
+  });
+
+
+  $(dobDay).on("change", () => console.log(dobDay.val()));
+  $(dobMonthNum).on("change", () => console.log(dobMonthNum.val()));
+  $(dobMonthName).on("change", () => console.log(dobMonthName.val()));
+  $(dobYear).on("change", () => console.log(dobYear.val()));
+
+  $(btn).on("click", () => {
+    event.preventDefault();
+    console.log();
+  })
 
 
   $(window).on("load", (() => {
@@ -30,5 +87,3 @@ $(function () {
                 }, 900));
 	}));
 });
-
-//.then(formText.css('white-space', 'normal'))
