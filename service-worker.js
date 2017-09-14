d2843a136379537eacc6186ba33a46ed41b953db
Majka Karpinska <maja.miri@gmail.com>
2017-09-14 15:56:03 +0200

    service worker file paths changes

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ const CACHE_NAME = 'xxx';
 
 // List of files which are store in cache.
 let filesToCache = [
-    '/',
-    '/css/style.css',
-    '/images/gredient.png',
-    '/js/app.js'
+    '/10clouds_form/',
+    '/10clouds_form/css/style.css',
+    '/10clouds_form/images/gredient.png',
+    '/10clouds_form/js/app.js'
 ];
 
 self.addEventListener('install', function (evt) {
