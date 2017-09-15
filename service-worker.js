5582439902a925c6d414a994eb531d3c5d73fac7
Majka Karpinska <maja.miri@gmail.com>
2017-09-15 14:39:06 +0200

    service worker updated, files saved to cache

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'aaa';
+
+// List of files which are store in cache.
+let filesToCache = [
+    '/',
+    '/css/style.css',
+    //'/images/logo.png',
+    '/js/jquery.js',
+    '/js/app.js'
+];
+
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(filesToCache);
+        }).catch(function (err) {
+            // Snooze errors...
+            console.error(err);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (evt) {
+    // Snooze logs...
+    // console.log(event.request.url);
+    evt.respondWith(
+        // Firstly, send request..
+        fetch(evt.request).catch(function () {
+            // When request failed, return file from cache...
+            return caches.match(evt.request);
+        })
+    );
+});
